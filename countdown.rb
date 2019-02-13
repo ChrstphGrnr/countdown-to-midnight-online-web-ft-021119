71643a1a13b459adc90d6ccd70abb0f1e5915d95
Christoph Gruener <christophgruener@me.com>
2019-02-13 17:40:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  number = integer
+  wile number > 0
+    puts "#{numer} SECOND(S)!"
+    number -= 1 
+  end
+  "HAPPY NEW YEAR!"
 end
